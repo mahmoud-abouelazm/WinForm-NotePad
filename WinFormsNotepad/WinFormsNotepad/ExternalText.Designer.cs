9ceabfae911eacb1d6b2b67ba668aa68a73a1a71
Mahmoud Reda <mahmoudreda20030@gmail.com>
2024-09-24 18:43:36 +0300

    Font button commit

---
+++ WinFormsNotepad/WinFormsNotepad/ExternalText.Designer.cs
@@ -0,0 +1,66 @@
+﻿namespace WinFormsNotepad
+{
+    partial class ExternalText
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(ExternalText));
+            btnOK = new Button();
+            richTextBox1 = new RichTextBox();
+            SuspendLayout();
+            // 
+            // btnOK
+            // 
+            resources.ApplyResources(btnOK, "btnOK");
+            btnOK.DialogResult = DialogResult.OK;
+            btnOK.Name = "btnOK";
+            btnOK.UseVisualStyleBackColor = true;
+            // 
+            // richTextBox1
+            // 
+            resources.ApplyResources(richTextBox1, "richTextBox1");
+            richTextBox1.Name = "richTextBox1";
+            // 
+            // ExternalText
+            // 
+            resources.ApplyResources(this, "$this");
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(richTextBox1);
+            Controls.Add(btnOK);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "ExternalText";
+            ShowInTaskbar = false;
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Button btnOK;
+        private RichTextBox richTextBox1;
+        
+    }
+}
