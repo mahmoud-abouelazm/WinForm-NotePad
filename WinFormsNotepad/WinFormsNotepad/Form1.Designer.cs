939344d33d867cc3a76aad4bb1e8d15c4ec4dd5e
Mahmoud Reda <mahmoudreda20030@gmail.com>
2024-09-24 18:43:17 +0300

    Font button commit

--- WinFormsNotepad/WinFormsNotepad/Form1.Designer.cs
+++ WinFormsNotepad/WinFormsNotepad/Form1.Designer.cs
@@ -28,12 +28,135 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            components = new System.ComponentModel.Container();
+            btnColor = new Button();
+            btnFont = new Button();
+            btnExternal = new Button();
+            dlgColor = new ColorDialog();
+            dlgFont = new FontDialog();
+            dlgOpen = new OpenFileDialog();
+            dlgSave = new SaveFileDialog();
+            toolTip1 = new ToolTip(components);
+            menuStrip1 = new MenuStrip();
+            openToolStripMenuItem = new ToolStripMenuItem();
+            saveToolStripMenuItem = new ToolStripMenuItem();
+            exitToolStripMenuItem = new ToolStripMenuItem();
+            txtForm = new RichTextBox();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // btnColor
+            // 
+            btnColor.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            btnColor.Font = new Font("Segoe UI", 12F);
+            btnColor.Location = new Point(12, 498);
+            btnColor.Name = "btnColor";
+            btnColor.Size = new Size(75, 40);
+            btnColor.TabIndex = 3;
+            btnColor.Text = "Color";
+            btnColor.UseVisualStyleBackColor = true;
+            // 
+            // btnFont
+            // 
+            btnFont.Anchor = AnchorStyles.Bottom;
+            btnFont.Font = new Font("Segoe UI", 12F);
+            btnFont.Location = new Point(248, 498);
+            btnFont.Name = "btnFont";
+            btnFont.Size = new Size(75, 40);
+            btnFont.TabIndex = 4;
+            btnFont.Text = "Font";
+            btnFont.UseVisualStyleBackColor = true;
+            btnFont.Click += btnFont_Click;
+            // 
+            // btnExternal
+            // 
+            btnExternal.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnExternal.Font = new Font("Segoe UI", 12F);
+            btnExternal.Location = new Point(482, 498);
+            btnExternal.Name = "btnExternal";
+            btnExternal.Size = new Size(106, 40);
+            btnExternal.TabIndex = 5;
+            btnExternal.Text = "External Text";
+            btnExternal.UseVisualStyleBackColor = true;
+            btnExternal.Click += btnExternal_Click;
+            // 
+            // dlgOpen
+            // 
+            dlgOpen.FileName = "openFileDialog1";
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { openToolStripMenuItem, saveToolStripMenuItem, exitToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(600, 24);
+            menuStrip1.TabIndex = 7;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // openToolStripMenuItem
+            // 
+            openToolStripMenuItem.Name = "openToolStripMenuItem";
+            openToolStripMenuItem.Size = new Size(48, 20);
+            openToolStripMenuItem.Text = "Open";
+            openToolStripMenuItem.Click += openToolStripMenuItem_Click;
+            // 
+            // saveToolStripMenuItem
+            // 
+            saveToolStripMenuItem.Name = "saveToolStripMenuItem";
+            saveToolStripMenuItem.Size = new Size(43, 20);
+            saveToolStripMenuItem.Text = "Save";
+            saveToolStripMenuItem.Click += saveToolStripMenuItem_Click;
+            // 
+            // exitToolStripMenuItem
+            // 
+            exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            exitToolStripMenuItem.Size = new Size(38, 20);
+            exitToolStripMenuItem.Text = "Exit";
+            // 
+            // txtForm
+            // 
+            txtForm.Location = new Point(12, 27);
+            txtForm.Name = "txtForm";
+            txtForm.Size = new Size(576, 465);
+            txtForm.TabIndex = 8;
+            txtForm.Text = "";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 17F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(600, 550);
+            Controls.Add(txtForm);
+            Controls.Add(btnExternal);
+            Controls.Add(btnFont);
+            Controls.Add(btnColor);
+            Controls.Add(menuStrip1);
+            Font = new Font("Segoe UI", 10F);
+            MainMenuStrip = menuStrip1;
+            MinimumSize = new Size(450, 504);
+            Name = "Form1";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Your Notepad";
+            FormClosing += Form1_FormClosing;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+        private Button btnColor;
+        private Button btnFont;
+        private Button btnExternal;
+        private ColorDialog dlgColor;
+        private FontDialog dlgFont;
+        private OpenFileDialog dlgOpen;
+        private SaveFileDialog dlgSave;
+        private ToolTip toolTip1;
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem openToolStripMenuItem;
+        private ToolStripMenuItem saveToolStripMenuItem;
+        private ToolStripMenuItem exitToolStripMenuItem;
+        private RichTextBox txtForm;
     }
 }
--- WinFormsNotepad/WinFormsNotepad/Form1.cs
+++ WinFormsNotepad/WinFormsNotepad/Form1.cs
@@ -1,10 +1,71 @@
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
 namespace WinFormsNotepad
 {
     public partial class Form1 : Form
-    {
+    {   
+            
+        ExternalText externalText = new ExternalText();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show(
+                "Sure you want to exit?" // message
+                , "Exit" // form title
+                , MessageBoxButtons.YesNo // form buttons
+                , MessageBoxIcon.Warning // type of form
+                , MessageBoxDefaultButton.Button2)
+                == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dlgOpen.Filter = "Rich file|*.rtf|Text file|*.txt";
+            if (dlgOpen.ShowDialog() == DialogResult.OK)
+            {
+                txtForm.LoadFile(dlgOpen.FileName, (RichTextBoxStreamType)(dlgOpen.FilterIndex - 1));
+            }
+        }
+
+        private void saveToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            dlgSave.Filter = "Rich file|*.rtf|Text file|*.txt";
+
+            if (dlgSave.ShowDialog() == DialogResult.OK)
+            {
+                txtForm.SaveFile(dlgSave.FileName, (RichTextBoxStreamType)(dlgSave.FilterIndex - 1));
+
+            }
+        }
+
+        private void btnFont_Click(object sender, EventArgs e)
+        {
+            dlgFont.Font = txtForm.SelectionFont;
+            if (dlgFont.ShowDialog() == DialogResult.OK)
+            {
+                txtForm.SelectionFont = dlgFont.Font;
+            }
+        }
+
+        private void btnExternal_Click(object sender, EventArgs e)
+        {
+            if(externalText.ShowDialog() == DialogResult.OK)
+            {
+                txtForm.Text = externalText.getString();
+            }
         }
     }
 }
